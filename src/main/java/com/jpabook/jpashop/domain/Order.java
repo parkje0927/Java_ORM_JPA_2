3dd3633f78cc4af4e20f83fdfb0d91d8f28d4ff0
parkje0927 <parkje09270@gmail.com>
2022-08-24 22:36:32 +0900

    [ADD] 단방향 연관관계 추가

--- src/main/java/com/jpabook/jpashop/domain/Order.java
+++ src/main/java/com/jpabook/jpashop/domain/Order.java
@@ -12,10 +12,45 @@ public class Order {
     @Column(name = "ORDER_ID")
     private Long id;
 
+    //이런 설계는 관계형 DB 에 맞춘 것이다. 좀 더 객체지향적 설계는 여기서 바로 Member 가 필드로 있어야 한다.
+//    private Member member;
+
     @Column(name = "MEMBER_ID")
     private Long memberId;
     private LocalDateTime orderDate; //최신 버전에서는 java8 LocalDate, LocalDateTime 자동 매핑
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(Long memberId) {
+        this.memberId = memberId;
+    }
+
+    public LocalDateTime getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(LocalDateTime orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
 }
